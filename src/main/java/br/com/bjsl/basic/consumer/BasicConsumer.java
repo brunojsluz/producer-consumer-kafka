bedaf7f2536aa6bf007f81de00ef2051699953c2
Bruno luz <brunojsluz@gmail.com>
2020-02-27 17:17:48 -0300

    Estrutura inicial

---
+++ src/main/java/br/com/bjsl/basic/consumer/BasicConsumer.java
@@ -0,0 +1,8 @@
+package br.com.bjsl.basic.consumer;
+
+public class BasicConsumer {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/br/com/bjsl/basic/producer/BasicProducer.java
@@ -0,0 +1,8 @@
+package br.com.bjsl.basic.producer;
+
+public class BasicProducer {
+
+    public static void main(String[] args) {
+
+    }
+}
